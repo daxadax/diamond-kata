eeccb28a80679c8ef0c21f213970920976ecfe16
Dax <d.dax@email.com>
2015-04-18 22:58:55 +0200

    Assert correct number of rows are returned

--- kata.rb
+++ kata.rb
@@ -2,10 +2,20 @@ class Kata
 
   def initialize(input)
     @input = input
+    @alphabet = ('a'..'z').to_a
+    @d_number = @alphabet.index(input)
   end
 
   def run
-    @input
+    total_lines = (@d_number * 2) + 1
+
+    diamond = total_lines.times.map do |i|
+      @input
+    end
+
+    diamond.each do |row|
+      puts row + "\n"
+    end
   end
 
 end
--- spec.rb
+++ spec.rb
@@ -9,9 +9,20 @@ class KataSpec < Minitest::Spec
   describe 'a' do
     let(:input) { 'a' }
 
+    it 'returns 1 row' do
+      assert_equal 1, result.size
+    end
+
     it 'returns "a"' do
-      assert_equal "a", result
+      assert_equal ["a"], result
     end
   end
-  
+
+  describe 'b' do
+    let(:input) { 'b' }
+
+    it 'returns 3 rows' do
+      assert_equal 3, result.size
+    end
+  end
 end
