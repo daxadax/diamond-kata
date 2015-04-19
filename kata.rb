c9004188c9e6186fea5b9f4da7086829458122a4
Dax <d.dax@email.com>
2015-04-19 20:24:15 +0200

    Refactor

--- kata.rb
+++ kata.rb
@@ -9,32 +9,37 @@ class Kata
 
   def run
     @grid.times.map do |i|
-      build_row_for(i)
+      build_row_for(determine_position(i))
     end
   end
 
   private
 
   def build_row_for(position)
-    if position > @alphabet_index
-      position -= (position - @alphabet_index) * 2
-    end
     character = @alphabet[position]
 
     if position.zero?
       character.center(@grid, '_')
     else
-      row = @grid.times.map do |i|
-        if @alphabet_index.even? && i.odd?
-          character
-        elsif @alphabet_index.odd? && i.even?
-          character
-        else
-          '_'
-        end
-      end
-      row.join
+      build_inner_row_for(character)
     end
+  end
+
+  def determine_position(position)
+    return position if position <= @alphabet_index
+    position - ((position - @alphabet_index) * 2)
+  end
+
+  def build_inner_row_for(character)
+    @grid.times.map do |i|
+      if @alphabet_index.even? && i.odd?
+        character
+      elsif @alphabet_index.odd? && i.even?
+        character
+      else
+        '_'
+      end
+    end.join
   end
 
 end
